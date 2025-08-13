78077a5e01c815e1bbf133cc2bd0b86506004ec4
Mayur Pal <thissidemayur@gmail.com>
2025-08-14 03:16:24 +0530

    add to github

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,18 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-};
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "www.pexels.com"
+      },
+      {
+        protocol: "https",
+        hostname: "randomuser.me"
+      },
+    ]
 
+  }
+};
 export default nextConfig;
